d2896be10fcb02d0fc9f179ee86a867b407f3aba
prashantbhoite123 <pbhoite985@gmail.com>
2024-10-03 09:55:37 +0530

    fixed logout api bug

--- src/Routes/userAuth.routes.ts
+++ src/Routes/userAuth.routes.ts
@@ -1,8 +1,9 @@
 import express from "express"
 import userAuthController from "../controllers/userAuth.controller"
+import { isAuthentication } from "../middleware/Auth.middleware"
 
 const router = express.Router()
 
-router.get("/logout", userAuthController.logoutUser)
+router.get("/logout", isAuthentication, userAuthController.logoutUser)
 
 export default router
