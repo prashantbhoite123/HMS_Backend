21ce934a0967fcf08d319eaf896842498dee5497
prashantbhoite123 <pbhoite985@gmail.com>
2024-11-20 11:29:56 +0530

    create and use single api and CREATE  approve and rejection api endpont

--- src/controllers/Admin/approvelHospital.controller.ts
+++ src/controllers/Admin/approvelHospital.controller.ts
@@ -45,40 +45,93 @@ const rejectHospitalApi = async (
     if (!reson) {
       return next(errorHandler(404, "reson is required"))
     }
-
-    const updatedhos = await Hospital.findByIdAndUpdate(
-      hospitalId,
-      {
-        $set: {
-          status: "Rejected",
+    // console.log(hospitalId)
+    if (reson === process.env.VITE_APPROVE) {
+      const approvedHos = await Hospital.findByIdAndUpdate(
+        hospitalId,
+        {
+          $set: {
+            status: "Approved",
+          },
         },
-      },
-      { new: true }
-    )
+        { new: true }
+      )
 
-    const hospitalOwner = await User.findOne({ _id: updatedhos?.owner })
-    if (!hospitalOwner) {
-      return next(errorHandler(404, "Hospital owner not found"))
-    }
-    sendMail(
-      // hospitalOwner?.email,
-      "pbhoite985@gmail.com",
-      // hospitalOwner?.email,
-      "bhoitep326@gmail.com",
-      "Hospital Registration Application Status",
-      `Dear ${hospitalOwner?.username}, \n\n
-       Thank you for your interest in registering with us !!. \n\n
-       ${reson} \n\n
-       ${updatedhos?.hospitalName}
+      const hospitalOwner = await User.findOne({ _id: approvedHos?.owner })
+      if (!hospitalOwner) {
+        return next(errorHandler(404, "Hospital owner not found"))
+      }
+      sendMail(
+        // hospitalOwner?.email,
+        "pbhoite985@gmail.com",
+        // hospitalOwner?.email,
+        "bhoitep326@gmail.com",
+        "Congratulations! Your Hospital is Approved",
+        `Dear ${hospitalOwner?.username}, \n\n
+       We are thrilled to share the fantastic news that your hospital, **${approvedHos?.hospitalName}**, has been successfully approved by our administration team!
+
+This milestone reflects your dedication and commitment to providing quality healthcare services. We are excited to have you as part of our esteemed network of hospitals. 
+
+We look forward to supporting you in delivering outstanding medical care to those in need. Should you have any questions or require assistance, please feel free to reach out to us.
+
+Once again, congratulations on this remarkable achievement!
+!. \n\n
+       
+       ${approvedHos?.hospitalName}
       `,
-      "",
-      process.env.EMAIL_USER,
-      process.env.EMAIL_PASS
-    )
-    console.log("hospital updated==>", updatedhos)
-    return res
-      .status(200)
-      .json({ success: true, message: "Hospital rejected !" })
+        "",
+        process.env.EMAIL_USER,
+        process.env.EMAIL_PASS
+      )
+      console.log("hospital updated==>", approvedHos)
+      return res
+        .status(200)
+        .json({ success: true, message: "Hospital Request Approved " })
+    } else {
+      const updatedhos = await Hospital.findByIdAndUpdate(
+        hospitalId,
+        {
+          $set: {
+            status: "Rejected",
+          },
+        },
+        { new: true }
+      )
+
+      const hospitalOwner = await User.findOne({ _id: updatedhos?.owner })
+      if (!hospitalOwner) {
+        return next(errorHandler(404, "Hospital owner not found"))
+      }
+      sendMail(
+        // hospitalOwner?.email,
+        "pbhoite985@gmail.com",
+        // hospitalOwner?.email,
+        "bhoitep326@gmail.com",
+        "Hospital Registration Application Status",
+
+        `
+      Dear ${hospitalOwner?.username}, 
+
+       Thank you for your interest in registering your hospital with us. After careful review, we regret to inform you that your application for **${updatedhos?.hospitalName}** has not been approved at this time.
+
+Reason for rejection:  
+${reson}
+
+We understand this may be disappointing news, and we encourage you to address the mentioned concerns or make any necessary improvements. You are welcome to reapply in the future once the changes have been made.
+
+If you have any questions or require further clarification, please do not hesitate to reach out to us.
+
+We value your efforts and look forward to the possibility of working with you in the future.
+      `,
+        "",
+        process.env.EMAIL_USER,
+        process.env.EMAIL_PASS
+      )
+      console.log("hospital updated==>", updatedhos)
+      return res
+        .status(200)
+        .json({ success: true, message: "Hospital rejected !" })
+    }
   } catch (error) {
     return next(error)
   }
