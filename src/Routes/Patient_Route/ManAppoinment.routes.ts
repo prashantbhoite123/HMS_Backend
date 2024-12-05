ade437f241e716c4640da9ba30dcb5769779d217
prashantbhoite123 <pbhoite985@gmail.com>
2024-12-05 12:22:13 +0530

    Add frontend Deploye URL

--- src/Routes/Patient_Route/ManAppoinment.routes.ts
+++ src/Routes/Patient_Route/ManAppoinment.routes.ts
@@ -17,7 +17,7 @@ router.put(
 
 router.get(
   "/search",
-  // isAuthentication,
+  isAuthentication,
   ManageAppoinment.searchAppoinment
 )
 
--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://hms-frontend-2f89.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
